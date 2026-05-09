55b6b3ba8f5e46fe932c8b12b8fea85e70f7468a
ocket8888 <ocket8888@gmail.com>
2026-05-09 00:13:10 -0600

    Add licensing

--- src/steamgridinstaller/api.py
+++ src/steamgridinstaller/api.py
@@ -1,3 +1,9 @@
+# Copyright 2026 ocket8888
+# This file is part of steamgridinstaller.
+# steamgridinstaller is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# steamgridinstaller is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with steamgridinstaller. If not, see <https://www.gnu.org/licenses/>. 
+
 import json
 import sys
 from typing import Final, Literal
