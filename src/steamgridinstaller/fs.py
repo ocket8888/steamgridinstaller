dbb3dc7f98e5097a4f3a2d4635d1f90201966423
ocket8888 <ocket8888@gmail.com>
2026-05-13 12:09:03 -0600

    Fix #2 - Clears existing assets on write

--- src/steamgridinstaller/fs.py
+++ src/steamgridinstaller/fs.py
@@ -2,7 +2,7 @@
 # This file is part of steamgridinstaller.
 # steamgridinstaller is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # steamgridinstaller is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with steamgridinstaller. If not, see <https://www.gnu.org/licenses/>. 
+# You should have received a copy of the GNU General Public License along with steamgridinstaller. If not, see <https://www.gnu.org/licenses/>.
 
 import os as _os
 from typing import AnyStr as _AnyStr, Final as _Final
@@ -26,7 +26,7 @@ class WriteAssetError(Exception):
 def getExtFromMime(mime: str) -> str:
 	if not mime.startswith("image/"):
 		raise TypeError(f"non-image or unrecognized MIME type: {mime}")
-	
+
 	try:
 		return _MIME_EXT_MAP[mime.split(";")[0].strip()]
 	except KeyError as e:
@@ -43,14 +43,14 @@ def locateOrCreateGridFolder(start: str, debug: bool) -> str:
 			if debug:
 				print("found steam user folder:", entry.path)
 			found = entry.path
-	
+
 	if found is None:
 		raise FileNotFoundError("no steam user folders exist in the given directory")
-	
+
 	found = _os.path.join(found, "config")
 	if not _os.path.isdir(found):
 		raise FileNotFoundError("steam user directory does not contain 'config' dir")
-	
+
 	found = _os.path.join(found, "grid")
 
 	if not _os.path.exists(found):
@@ -60,11 +60,23 @@ def locateOrCreateGridFolder(start: str, debug: bool) -> str:
 
 	return found
 
-def writeAsset(asset: _Asset, id: int, dir: str, debug: bool, isLogo: bool = False):
+def eraseExisting(fname: str, debug: bool):
+	"""
+	Erases any existing files that correspond to the same game as `fname`.
+	"""
+	base = fname.removesuffix(".jpg").removesuffix(".png")
+	for ext in ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"]:
+		existing = f"{base}.{ext}"
+		if _os.path.exists(existing):
+			if debug:
+				print("removing existing file:", existing)
+			_os.remove(existing)
+
+def writeAsset(asset: _Asset, id: int, path: str, debug: bool, isLogo: bool = False):
 	"""
 	Writes the given asset to a steam grid file.
 	"""
-	fname = _os.path.join(dir, f"{id}")
+	fname = _os.path.join(path, f"{id}")
 	if isLogo:
 		if debug:
 			print("game", id, f"({asset.game.name}) is logo")
@@ -79,12 +91,12 @@ def writeAsset(asset: _Asset, id: int, dir: str, debug: bool, isLogo: bool = Fal
 		fname = f"{fname}_hero"
 	elif (asset.width != 920 or asset.height != 430) and (asset.width != 460 or asset.height != 215):
 		raise WriteAssetError(f"unsupported dimensions for game asset: {asset.height}x{asset.width}")
-	
+
 	try:
 		fname = f"{fname}{getExtFromMime(asset.mime)}"
 	except TypeError as e:
 		raise WriteAssetError(f"failed to get extension from mime type '{asset.mime}': {e}") from e
-	
+
 	url = asset.url
 	if asset.animationType == "WebP":
 		if asset.fakePng is None:
@@ -100,8 +112,9 @@ def writeAsset(asset: _Asset, id: int, dir: str, debug: bool, isLogo: bool = Fal
 		data = _requests.get(url).content
 	except (_requests.exceptions.RequestException, IOError) as e:
 		raise WriteAssetError(f"failed to fetch asset data: {e}") from e
-	
+
 	try:
+		eraseExisting(fname, debug)
 		if debug:
 			print("writing asset to file:", fname)
 		with open(fname, 'wb') as fd:
